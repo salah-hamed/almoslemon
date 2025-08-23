b98497647607a93af9ea295c7616a820e0ca3ca8
salah-hamed <salahhamed236@gmail.com>
2025-08-23 17:35:33 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,3 +3,5 @@ const urlsToCache = [ "/", "/index.html", "/manifest.json", "/about.html", "/pri
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keyList => Promise.all(keyList.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); });
 self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(response => response || fetch(e.request))); });
+const todayId = new Date().toISOString().split('T')[0]; // "YYYY-MM-DD"
+const colName = 'global_counts_daily'; // أو 'global_counts_daily' لو غيّرت الاسم
